e39aa71d8aba5be305d3f9c771eac83b608ac67f
Shayla Luong <luongnh1@uci.edu>
2022-03-22 00:56:58 -0700

    update visual elements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # uid-asl-learning
+
+2D image assets from https://pixelfrog-assets.itch.io/pixel-adventure-1
--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 var block = document.getElementById("block");
 var character = document.getElementById("character");
 var curr = 0;
+var charPos = 0;
 var keyList = [76, 67, 66, 79, 90];
 var letterList = ["L","C","B","O","Z"];
-var srcList = ["src/L_letter.jpg","src/C_letter.jpg","src/B_letter.jpg","src/O_letter.jpg","Z"];
-
-// function move(){
-//     // if(character.classList != "animate"){
-//     // character.classList.add("animate");}
-//     // character.style.left = parseInt(character.style.left, 10) + 80 + "px";
-//     character.style.left = character.style.left + "80px";
-//     // setTimeout(function(){character.classList.remove("animate");},500);
-// }
+var srcList = ["src/L_letter.jpg","src/C_letter.jpg","src/B_letter.jpg","src/O_letter.jpg","src/Z_letter.jpg"];
 
 function moveDown() {
     var keycode = window.event.keyCode;
     if (keycode == keyList[curr]) {
-        var obj = document.getElementById("box");
+        var obj = document.getElementById("character");
+        // add animation
+        if(obj.classList != "animate"){
+        obj.classList.add("animate");}
+        // update character's animation position
+        document.documentElement.style.setProperty('--char-pos', charPos+'px');
+        charPos += 80;
+        // update character's fixed location
         var leftVal = parseInt(obj.style.left, 10);
-        obj.style.left = (leftVal + 100) + "px";    
-        curr += 1;
+        obj.style.left = (leftVal + 80) + "px";    
+        // remove animation
+        setTimeout(function(){obj.classList.remove("animate");},500);
 
+        curr += 1;
+        // update ASL display letter
         var letterText = document.getElementById("letter");
         letterText.textContent = letterList[curr];
-        
+        // update ASL display image
         var imgObj = document.getElementById("image");
         imgObj.src = srcList[curr];
     }
