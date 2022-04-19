8ab6a3f802feb87c9a455ed3037c78a4134ceea5
Shayla Luong <luongnh1@uci.edu>
2022-04-19 13:26:57 -0700

    port tensorflow model to js and create working prediction prototype

--- script.js
+++ script.js
@@ -1,12 +1,75 @@
-var curr = 0;
+const STATUS = document.getElementById('status');
+
+var model;
+
+// setup for video capture
+var video = document.querySelector("#webCamera");
+video.onplay = function() {
+    setTimeout(drawBoundingBox , 300);
+};
+
+// setup for button clicks
+document.getElementById("startButton").addEventListener("click", startGame, false);
+
+// // setup for game variablesvar curr = 0;
 var charPos = 0;
 var keyList = [76, 67, 66, 79, 78];
 var letterList = ["L","C","B","O","N"];
 var srcList = ["src/L_letter.jpg","src/C_letter.jpg","src/B_letter.jpg","src/O_letter.jpg","src/N_letter.jpg"];
 
+async function loadASLModel() {
+    model = await tf.loadLayersModel('models/model.json');
+    STATUS.innerText = 'MobileNet v3 loaded successfully!';
+}
+
+loadASLModel();
+
+function loadCamera(){        
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({audio: false, video: {facingMode: 'user'}})
+        .then( function(stream) {
+            video.srcObject = stream;
+            return stream;            
+        })
+        .catch(function(error) {
+            alert("Error opening device");
+        });
+    }
+}
+
+function drawBoundingBox(){
+    var video = document.querySelector("#webCamera");
+    var canvas = document.querySelector("#videoCanvas");
+    var ctx = canvas.getContext('2d');
+
+    // canvas.width = video.videoWidth;
+    // canvas.height = video.videoHeight;
+    
+    canvas.width = "320";
+    canvas.height = "240";
+    
+
+    ctx.translate(canvas.width, 0);
+    ctx.scale(-1, 1);
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    var frameImg = ctx.getImageData(20, 20, 128, 128)
+    predictASL(frameImg);
+
+    ctx.rect(172, 20, 128, 128);
+    ctx.lineWidth = "6";
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+
+    setTimeout(drawBoundingBox, 100);
+}
+
 function startGame() {
     document.getElementById("startScreen").style.setProperty("display", "none");
     document.getElementById("gameScreen").style.setProperty("display", "inline");
+    const STATUS = document.getElementById('status');
+
+    loadCamera();
 }
 
 function finishGame() {
@@ -41,4 +104,29 @@ function updateState() {
         var imgObj = document.getElementById("image");
         imgObj.src = srcList[curr];
     }
+}
+
+function getChar(x) {
+    let y = 65+x;
+    if (5<=x && x<=14) return String.fromCharCode(y-1);
+    else if (16<=x && x<=20) return String.fromCharCode(y-2);
+    else if (x>=22) return String.fromCharCode(y-3);
+    else if (x<=3) return String.fromCharCode(y);
+    else return "nothing";
+}
+
+function predictASL(imgFrame) {
+    // let img_inp = document.getElementById("img_inp");
+    let imgTensor = tf.browser.fromPixels(imgFrame);
+    
+    // convert image to grayscale
+    imgTensor = imgTensor.mean(2).toFloat().expandDims(0).expandDims(-1);
+    imgTensor = imgTensor.div(255);
+    const alignCorners = true;
+    const imgResize = tf.image.resizeBilinear(imgTensor, [64, 64], alignCorners);
+
+    let prediction = model.predict(imgResize).squeeze();
+    let highestIndex = prediction.argMax().arraySync();
+
+    document.getElementById("predText").textContent = "prediction: " + getChar(highestIndex);
 }
