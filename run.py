98835f560baa39ef06c3e520d38b1543aab75c77
Shayla Luong <shayla.luong@sjsu.edu>
2022-05-20 21:47:00 -0700

    add python script to run local server

---
+++ run.py
@@ -0,0 +1,27 @@
+import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+import webbrowser
+
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+Handler.extensions_map={
+        '.manifest': 'text/cache-manifest',
+	'.html': 'text/html',
+        '.png': 'image/png',
+	'.jpg': 'image/jpg',
+	'.svg':	'image/svg+xml',
+	'.css':	'text/css',
+	'.js':	'application/x-javascript',
+	'': 'application/octet-stream', # Default
+    }
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("serving at port", PORT)
+webbrowser.open('http://localhost:8000/', new=2)
+httpd.serve_forever()
+
